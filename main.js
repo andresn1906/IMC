6d3f41bdda6fcb8531767428ae884cae662839ea
Andrés Suárez <andresuarez1906@gmail.com>
2025-05-01 11:20:06 -0500

    Commented code js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+// Bloque para llamar a las variables por su id y tomar su visualViewport, crear la función para calcular el IMC y los mensajes
 function calcularImc(){
     const nombre = document.getElementById("tName").value;
     const peso = document.getElementById("nWeight").value;
@@ -7,14 +7,14 @@ function calcularImc(){
     const imc = peso / (altura * altura);
     const result = document.getElementById("tResultado");
     const imagen = document.getElementById("imagen");
-
     result.innerHTML = `${nombre} su IMC es:  ${imc.toFixed(2)}`;
 
+/* Bloque para validar los datos ingresados por el usuario a través 3 ifs, el primero verifica si los campos están vacíos, el segundo si se han ingresado valores númericos posibles y el tercer "if" contiene las acciones a realizar dependiendo del resultado del IMC que obtenga la persona.Bloque */
     if (!nombre ||!peso || !altura) {
         alert("Por favor, llene los espacios en blanco.");
         reiniciarImc(); 
         return;
-    }
+    } 
     if ( altura === 0 || peso === 0 || isNaN(peso) || isNaN(altura)) {
         alert("Por favor, digite los valores correctamente.");
         reiniciarImc(); 
@@ -39,10 +39,12 @@ function calcularImc(){
     } else {
         result.innerHTML += "<br>Digite correctamente sus datos de peso/estatura.";
     }
+    // Bloque para que una vez realizado las acciones del if y obtenido el resultado, se oculte el botón "Calcular" y se muestre el botón "Reiniciar".
     document.getElementById("calculate").style.display = "none";
     document.getElementById("restart").style.display = "inline";   
 }   
 
+/* Bloque para crear la función de Reiniciar IMC, dónde se llaman a las variables por su id y se les asigna un valor vacío, además de ocultar la imagen y los botones de calcular y reiniciar. */
 function reiniciarImc() {
     document.getElementById("tName").value = "";
     document.getElementById("nAge").value = "";
